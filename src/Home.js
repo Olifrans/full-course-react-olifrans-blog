0eb2e349ec13b5b73e0ca3a28b93e3facb4e3b7e
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-04 14:33:01 -0300

    mod-blog

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,43 @@
 import { useState } from "react";
 
 const Home = () => {
-
-
-  const [blog, setBlog] = useState([
-    {title: "Meu site teste", bodY: "Tecnológia da informação", author: "olifrans", id: 1}
-    {title: "Meu site teste", bodY: "Tecnológia da informação", author: "pedro", id: 2}
-    {title: "Meu site teste", bodY: "React e DotNet", author: "mario", id: 3}
-
-  ])
-
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Meu site teste",
+      bodY: "Tecnológia da informação",
+      author: "Olifrans",
+      id: 1,
+    },
+    {
+      title: "Meu site teste",
+      bodY: "Tecnológia da informação",
+      author: "Pedro",
+      id: 2,
+    },
+    {
+      title: "Meu site teste",
+      bodY: "React e DotNet",
+      author: "Mario André",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
       <p> {Math.random() * 1.666666666} </p>
       <h2>ITI - Instituto Tecnológico Inovação</h2>
+
+
+      {blogs.map((blog)=>(
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Esta mensagem foi enviada de: {blog.author}</p>
+        </div>
+
+      ))}
+
+
+
     </div>
   );
 };
