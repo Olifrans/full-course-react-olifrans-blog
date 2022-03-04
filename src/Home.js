d00b841067b25af5a451132e99425cecbd02884e
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-04 15:28:20 -0300

    filter use

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,34 @@ import ListarBlog from "./ListarBlog";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {
-      title: "Meu Teste - 01",
-      bodY: "Tecnológia da informação",
+      title: "Curso de MPython",
+      bodY: "Python com BI",
       author: "Olifrans",
       id: 1,
     },
     {
-      title: "Meu Teste - 02",
+      title: "Curso de Java Sql-Server",
       bodY: "Java EE e Sql Server",
       author: "Pedro",
       id: 2,
     },
     {
-      title: "Meu Teste - 03",
+      title: "Curso de  Back e Front End",
       bodY: "React e DotNet",
       author: "Mario André",
       id: 3,
+    },
+    {
+      title: "Curso de Agile",
+      bodY: "Agile com React e DotNet",
+      author: "Olifrans",
+      id: 4,
+    },
+    {
+      title: "Curso de  Scrum",
+      bodY: "Scrum e DotNet",
+      author: "Olifrans",
+      id: 5,
     },
   ]);
 
@@ -27,8 +39,13 @@ const Home = () => {
     <div className="home">
       <p> {Math.random() * 1.666666666} </p>
       <h2>ITI - Instituto Tecnológico Inovação</h2>
-      <br></br>      
+      <br></br>
       <ListarBlog blogs={blogs} title="Área de Mensagens!" />
+
+      <ListarBlog
+        blogs={blogs.filter((blog) => blog.author === "Olifrans")}
+        title="Mensagens de Olifrans!"
+      />
     </div>
   );
 };
